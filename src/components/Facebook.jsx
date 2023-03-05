218ecedbfdb0fd4b99ea86e33fdf5ffc57867f38
Pau Bertolin <paulaberos@gmail.com>
2023-03-05 21:36:05 +0100

    iteration 14 - bonus 1

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from 'react';
 
-const Facebook = ({profileInfo}) => {
-  
+const Facebook = ({ profileInfo }) => {
+  const [usersInfo, setUsersInfo] = useState(profileInfo);
+  const [buttonSelected, setButtonSelected] = useState("");
+  const [showHide, setShowHide] = useState(false);
+  const [firstName, setFirstName] = useState("")
 
+  const noRepeatCountries = [];
+  profileInfo.filter(elem => !noRepeatCountries.includes(elem.country) ? noRepeatCountries.push(elem.country) : elem);
+  
+  const handleButtonSelected = (country) => {
+    setButtonSelected(country)
+  };
+
+  const handleHideShowInfo = (name) => {
+    setShowHide(prev => !prev)
+    setFirstName(name)
+  };
+  
   return (
     <>
-      {profileInfo.map((elem, i) => {
+      <section className="fb-buttons">
+      {noRepeatCountries.map((elem, i) => {
         return (
-          <section className="user-section" key={i}>
-            <div className="photo-div">
+          <button key={`button-${i}`} onClick={()=> {handleButtonSelected(elem)}} className={elem===buttonSelected ? "blue-button" : "white-button"}>{elem}</button>
+        )
+      })};
+        </section>
+      {usersInfo.map((elem, i) => {
+        return (
+          <section onChange={() => handleButtonSelected(elem.country)} className={elem.country === buttonSelected ? "blue-background user-section" : "white-background user-section" } key={i}> 
+            <div className="photo-div" onClick={() => handleHideShowInfo(elem.firstName)}>
               <img src={elem.img} alt={elem.firstName} className="user-pic"/>
             </div>
-            <div className = "info-user-div">
+            {firstName === elem.firstName ? <div className="info-user-div">
               <p><span>First Name:</span>{elem.firstName}</p>
               <p><span>Last Name:</span>{elem.lastName}</p>
               <p><span>Country:</span>{elem.country}</p>
               <p><span>Type:</span>{elem.isStudent ? "Student" : "Teacher"}</p>
-            </div>
+              </div> : null} 
           </section>
         )
       })}
